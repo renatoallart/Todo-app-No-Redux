3e0a499ed891e0bba66148ee06e06409766ed14f
renatoallart <renato.senstec@gmail.com>
2020-04-08 12:03:45 -0300

    adding midware Cors

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -3,9 +3,11 @@ const port  = 3003
 const bodyParser = require('body-parser')
 const express = require('express')
 const server = express()
+const cors = require('./cors')
 
 server.use(bodyParser.urlencoded({extended : true}))
 server.use(bodyParser.json())
+server.use(allowCors)
 
 server.listen(port, function(){
     console.log(`Backend is running on port ${port}.`)
